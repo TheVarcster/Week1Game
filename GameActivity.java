c6bba68946156f42819984e54aea478516d900fa
andreas <thevarcster@gmail.com>
2017-02-08 11:33:18 -0500

    added project files to repo

---
+++ GameActivity.java
@@ -0,0 +1,41 @@
+package andreasphillips.week1game.gdf.mgms.fullsail.edu.week1game;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class GameActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(event.getAction() == event.ACTION_DOWN) {
+            //Intent i = new Intent(this, GameActivity.class);
+            //i.putExtra("GameMode", 0);
+            //startActivity(i);
+            //finish();
+        }
+        return false;
+    }
+}
