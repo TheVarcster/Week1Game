e52f739f94fb5b7da2f13436889e52799aa9e406
andreas <thevarcster@gmail.com>
2017-02-10 18:20:31 -0500

    added project files to repo

---
+++ Constants.java
@@ -0,0 +1,6 @@
+package andreasphillips.week1game.gdf.mgms.fullsail.edu.week1game;
+
+public class Constants {
+public static String apiKey = "00f366e066ae408f7839b9f6c6cc93180cb9085400b894014ee78fe78f0cd9fa";
+public static String secretKey = "24e47f9bcf8236f8f0f7b232056e80ff17aa66ea79b1e85f28c16883457d76af";
+}
